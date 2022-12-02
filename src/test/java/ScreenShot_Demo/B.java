8569525575a1d8a64d2cea01df6248bcd79cb8f0
YogeshKawar <ykawar00@gmail.com>
2022-12-02 12:41:20 +0530

    changes

---
+++ src/test/java/ScreenShot_Demo/B.java
@@ -0,0 +1,28 @@
+package ScreenShot_Demo;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.bytebuddy.utility.RandomString;
+
+public class B {
+public static void main(String[] args) throws Throwable {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com/");
+	
+	String RS = RandomString.make(4);
+	
+	File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	File dest = new File("src/main/resources/Screenshots"+RS+".jpg");
+	
+	FileHandler.copy(src, dest);
+}
+}
