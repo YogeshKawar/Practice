234e4d4bc90b058a2109ac38edac0c8456961ae6
YogeshKawar <ykawar00@gmail.com>
2022-12-02 12:32:34 +0530

    Changes

--- src/test/java/ScreenShot_Demo/RandomName.java
+++ src/test/java/ScreenShot_Demo/RandomName.java
@@ -1,6 +1,8 @@
 package ScreenShot_Demo;
 
 import java.io.File;
+import java.io.IOException;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -8,15 +10,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import net.bytebuddy.utility.RandomString;
 
-public class A {
+public class RandomName {
 public static void main(String[] args) throws Throwable {
 	WebDriverManager.chromedriver().setup();
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.amazon.in/");
 	
+	String RS = RandomString.make(4);
+	
 	File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-	File dest = new File("C:\\Eclipse Workspace\\StartToEnd\\src\\main\\resources\\Screenshots\\amazon.jpg");
+	File dest = new File("C:\\Eclipse Workspace\\StartToEnd\\src\\main\\resources\\Screenshots\\"+RS+".jpg");
 	
 	FileHandler.copy(src, dest);
 }
